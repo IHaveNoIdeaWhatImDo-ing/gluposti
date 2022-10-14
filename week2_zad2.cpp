ed63b5e4a711dbd4ae4e91a4941a63f00963cc6e
IHaveNoIdeaWhatImDo-ing <84041839+IHaveNoIdeaWhatImDo-ing@users.noreply.github.com>
2022-10-14 09:00:04 +0300

    Create week2_zad2.cpp

---
+++ week2_zad2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int num1, num2;
+    cin >> num1 >> num2;
+    
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+    cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+
+    return 0;
+}
