9fec30cd381d02bd606a29640416d9e4a4eb9aa7
IHaveNoIdeaWhatImDo-ing <84041839+IHaveNoIdeaWhatImDo-ing@users.noreply.github.com>
2022-10-28 08:46:38 +0300

    Create zad1.cpp

---
+++ week4_friday/zad1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int month, year, days;
+    cout << "Enter a month and year: ";
+    cin >> month >> year;
+    
+    bool leap_year = ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
+    
+    if (month > 0 && month < 13)
+    {
+        if (month == 2)
+        {
+            days = leap_year ? 29 : 28;
+        }
+        else
+        {
+            days = (month % 2 == 0) ? 30 : 31;
+        }
+    }
+    
+    cout << "The given month has " << days << ".";
+
+    return 0;
+}
