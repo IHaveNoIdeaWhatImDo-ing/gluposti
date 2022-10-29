e8866c8182e56e5cc396c8d4a8c1ace184c951e5
IHaveNoIdeaWhatImDo-ing <84041839+IHaveNoIdeaWhatImDo-ing@users.noreply.github.com>
2022-10-29 06:20:17 -0700

    Update zad5.cpp

--- week4_friday/zad5.cpp
+++ week4_friday/zad5.cpp
@@ -52,9 +52,89 @@ int main()
     }
     
     // task 6
-    // unfinished
-  
-    cout << "The condition is " << (condition ? "true." : "false.");
+    int x, y;
+    cin >> x >> y;
+    
+    if (!(sqrt(x * x + y * y) < 10 && sqrt(x * x + y * y) > 5))
+    {
+        condition = false;
+    }
+    
+    // task 7
+    double x_double;
+    cin >> x_double;
+    if (!(x_double >= 0 && x_double <= 1))
+    {
+        condition = false;
+    }
+    
+    // task 10 (за 8 и 9 не разбрах условията)
+    bool uslovie1, uslovie2;
+    cin >> uslovie1 >> uslovie2;
+    if (!uslovie1 && !uslovie2)
+    {
+        condition = false;
+    }
+    
+    // task 11
+    cin >> uslovie1 >> uslovie2;
+    if (!uslovie1 || !uslovie2)
+    {
+        condition = false;
+    }
+    
+    // task 12
+    cin >> a >> b >> c;
+    if (a >= 0 || b >= 0 || c >= 0)
+    {
+        conditiion = false;
+    }
+    
+    // task 13
+    cin >> a;
+    
+    int digit1 = a % 10;
+    a /= 10;
+    int digit2 = a % 10;
+    a /= 10;
+    int digit3 = a % 10;
+    
+    if (digit1 != 7 && digit2 != 7 && digit3 != 7)
+    {
+        condition = false;
+    }
+    
+    // task 14
+    cin >> a;
+    
+    int digit1 = a % 10;
+    a /= 10;
+    int digit2 = a % 10;
+    a /= 10;
+    int digit3 = a % 10;
+    
+    if (digit1 == digit2 || digit2 == digit3 || digit1 == digit3)
+    {
+        condition = false;
+    }
+    
+    // task 15
+    cin >> a;
+    
+    int digit1 = a % 10;
+    a /= 10;
+    int digit2 = a % 10;
+    a /= 10;
+    int digit3 = a % 10;
+    
+    bool same_digits = digit1 == digit2 || digit2 == digit3 || digit1 == digit3;
+    
+    if (!same_digits)
+    {
+        condition = false;
+    }
+    
+    cout << "The conditions are " << (condition ? "true." : "false.");
 
     return 0;
 }
