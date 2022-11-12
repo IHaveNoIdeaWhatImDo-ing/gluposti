fad2b4f32c917f04de1c3b72be208ca5c53de0a3
IHaveNoIdeaWhatImDo-ing <84041839+IHaveNoIdeaWhatImDo-ing@users.noreply.github.com>
2022-11-12 01:22:15 -0800

    Create zad5.cpp

---
+++ week6/zad5.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+int power (int number, int powah)
+{
+    int multiplier = number;
+    
+    for (int i = 0; i < powah - 1; ++i)
+    {
+        number *= multiplier;
+    }
+    
+    return number;
+}
+
+bool isPower (int number1, int number2)
+{
+    int stepen = 1;
+    while  (power(number1, stepen) <= number2)
+    {
+        if (power(number1, stepen) == number2)
+            return true;
+        stepen++;
+    }
+    
+    return false;
+}
+
+int powah (int number1, int number2)
+{
+    int stepen = number1, counter = 1;
+    while (number1 != number2)
+    {
+        number1 *= stepen;
+        counter++;
+    }
+    
+    return counter;
+}
+
+int main()
+{
+    int number1, number2;
+    do 
+    {
+        cout << "Enter 2 positive numbers, where the second one is larger than the first one: ";
+        cin >> number1 >> number2;
+    }
+    while (number1 <= 0 || number2 < number1);
+    
+    if (isPower(number1, number2))
+        cout << "The number " << number1 << " is " << number2 << " rased to the power of " << powah(number1, number2) << ".\n";
+    else
+        cout << "The number " << number1 << " is not a power of the number " << number2 << ".\n";
+
+    return 0;
+}
