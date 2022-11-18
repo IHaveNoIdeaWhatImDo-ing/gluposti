cc55e03f693cccf723a292865b1528cf830f1ab9
IHaveNoIdeaWhatImDo-ing <84041839+IHaveNoIdeaWhatImDo-ing@users.noreply.github.com>
2022-11-18 08:29:34 +0200

    Create zad1.cpp

---
+++ week7/zad1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int ARR_SIZE = 100;
+
+int main()
+{
+    int N;
+    do
+    {
+        cout << "Enter a valid array size, between 1 and 100: ";
+        cin >> N;
+    }
+    while (N <= 0 || N > ARR_SIZE);
+    int arr[ARR_SIZE] = {};
+    
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> arr[i];
+    }
+    
+    for (int i = 0; i < N; ++i)
+    {
+        if (arr[i] % 2 == 0)
+            cout << arr[i] << " ";
+    }
+
+    return 0;
+}
