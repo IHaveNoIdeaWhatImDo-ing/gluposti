9c2b4dc08be053afce553d9029799760dbc3f7f9
IHaveNoIdeaWhatImDo-ing <84041839+IHaveNoIdeaWhatImDo-ing@users.noreply.github.com>
2022-11-29 12:01:10 -0800

    Create zad2.cpp

---
+++ week8/zad2.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+void matrixMult (int matrix1[][MAX_SIZE], int matrix2[][MAX_SIZE], int newMatrix[][MAX_SIZE], int row1, int col1)
+{
+    for (int x = 0; x < row1; ++x)
+    { 
+        for (int y = 0; y < row1; ++y)
+        {
+            int currentElement = 0;
+            for (int i = 0; i < col1; ++i)
+            {
+                currentElement += matrix1[x][i] * matrix2[i][y];
+            }
+            
+            newMatrix[x][y] = currentElement;
+        }
+    }
+}
+
+int main()
+{
+    int rows1, columns1, rows2, columns2;
+    
+    do {
+        cout << "Enter valid width and height for the first and the second matrix: ";
+        cin >> rows1 >> columns1 >> rows2 >> columns2;
+    } while ( rows1 <= 0 || rows1 > 100 || columns2 != rows1 ||
+              rows2 <= 0 || rows2 > 100 || columns1 != rows2 );
+    
+    int matrix1[MAX_SIZE][MAX_SIZE],
+        matrix2[MAX_SIZE][MAX_SIZE],
+        newMatrix[MAX_SIZE][MAX_SIZE];
+        
+    for (int x = 0; x < rows1; ++x)
+    {
+        cout << "You're currently entering the elements of row " << x + 1 << " of the first matrix.\n";
+        for (int y = 0; y < columns1; ++y)
+        {
+            cin >> matrix1[x][y];
+        }
+    }
+    cout << endl;
+    
+    for (int x = 0; x < rows2; ++x)
+    {
+        cout << "You're currently entering the elements of row " << x + 1 << " of the second matrix.\n";
+        for (int y = 0; y < columns2; ++y)
+        {
+            cin >> matrix2[x][y];
+        }
+    }
+    cout << endl;
+    
+    matrixMult(matrix1, matrix2, newMatrix, rows1, columns1);
+    
+    for (int x = 0; x < rows1; ++x)
+    {
+        for (int y = 0; y < rows1; ++y)
+        {
+            cout << newMatrix[x][y] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
