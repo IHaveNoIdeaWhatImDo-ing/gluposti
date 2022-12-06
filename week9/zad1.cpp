428517adcd59688b46eb9721e117238a3df40aad
IHaveNoIdeaWhatImDo-ing <84041839+IHaveNoIdeaWhatImDo-ing@users.noreply.github.com>
2022-12-06 14:30:02 +0200

    Update zad1.cpp

--- week9/zad1.cpp
+++ week9/zad1.cpp
@@ -8,6 +8,13 @@ void intSwap (int *num1, int *num2)
     *num1 = *num2;
     *num2 = c;
 }
+
+void doubleSwap (double *num1, double *num2)
+{
+    double c = *num1;
+    *num1 = *num2;
+    *num2 = c;
+}
     
 int main()
 {
