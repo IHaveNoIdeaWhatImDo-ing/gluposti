c6be7a9fdeed9d31360eb21b59b7232dbc104ef5
IHaveNoIdeaWhatImDo-ing <84041839+IHaveNoIdeaWhatImDo-ing@users.noreply.github.com>
2022-12-13 13:55:39 -0800

    Update zadacha1.cpp

--- test_up/zadacha1.cpp
+++ test_up/zadacha1.cpp
@@ -1,65 +1,138 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 const int MAX_SIZE = 100;
 
-void copy (int arr[][MAX_SIZE], int new_arr[][MAX_SIZE], int length)
+double validCharacters (string& str)
 {
-    for (int x = 0; x < length; ++x)
+    double counter = 0;
+    for (int i = 0; i < str.length(); ++i)
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= '0' && str[i] <= '9'))
+            counter++;
+    
+    return counter;
+}
+
+void flip (char matrix[][MAX_SIZE], int& key, int& rows)
+{
+    char bufferMatrix[MAX_SIZE][MAX_SIZE];
+    
+    for (int y = 0; y < key; ++y)
     {
-        for (int y = 0; y < length; ++y)
+        for (int x = 0; x < rows; ++x)
         {
-            arr[x][y] = new_arr[length - x - 1][y];
+            bufferMatrix[y][rows - x - 1] = matrix[x][y];
+        }
+    }
+    
+    for (int x = 0; x < key; ++x)
+    {
+        for (int y = 0; y < rows; ++y)
+        {
+            matrix[x][y] = bufferMatrix[x][y];
+        }
+    }
+    int temp = rows;
+    rows = key;
+    key = temp;
+}
+
+void copyMessageToMatrix (string& message, char matrix[][MAX_SIZE], int& key, int& rows, bool& positive)
+{
+    int length = message.length(), index = 0, x = 0, y = 0;
+    for (int i = 0; i < length; ++i)
+    {
+        if ((message[i] >= 'a' && message[i] <= 'z') || (message[i] >= '0' && message[i] <= '9'))
+        {
+            // Превръщам индекса на едномерния масив във x и y индекси на двумерен масив
+            x = index / key,
+            y = index % key;
+            index++;
+            
+            if (positive)
+                matrix[x][y] = toupper(message[i]);
+            else
+                matrix[key - x - 1][rows - y - 1] = toupper(message[i]);
+            
+            // Fix the negative key thing
+        }
+    }
+    
+    // Допопълвам празните елементи, ако има
+    for (int w = x; w < rows; ++w)
+    {
+        for (int h = y; h < key; ++h)
+        {
+            if (matrix[w][h] == 0)
+            {
+                if (positive)
+                    matrix[w][h] = 'X';
+                else
+                    matrix[rows - w - 1][key - h - 1] = 'X';
+            }
         }
     }
 }
 
-void flipClockwise (int arr[][MAX_SIZE], int new_arr[][MAX_SIZE], int length)
+string encrypt (char matrix[][MAX_SIZE], int& key, int& rows)
 {
-    for (int x = 0; x < length; ++x)
+    string encryptedStr = "";
+    
+    int layer, smaller = (key > rows ? rows : key);
+    for (layer = 0; layer < smaller / 2; ++layer)
     {
-        for (int y = 0; y < length; ++y)
+        for (int rotation = 0; rotation < 4; ++rotation)
         {
-            new_arr[y][x] = arr[x][y];
+            for (int i = layer; i < rows - 1 - layer; ++i)
+            {
+                encryptedStr += matrix[i][layer];
+            }
+            flip(matrix, key, rows);
         }
     }
-    copy(arr, new_arr, length);
+    
+    if (smaller % 2 != 0)
+    {
+        for (int i = layer; i < rows - layer; ++i)
+        {
+            encryptedStr += matrix[i][layer];
+        }
+    }
+    
+    return encryptedStr;
 }
 
 int main()
 {
-    int size;
-    do {
-        cout << "Enter a valid size for the square matrix: ";
-        cin >> size;
-    } while (size <= 0 || size > MAX_SIZE);
-    cout << "\n";
-
-    int grid[MAX_SIZE][MAX_SIZE],
-        grid2[MAX_SIZE][MAX_SIZE];
-
-    for (int x = 0; x < size; ++x)
+    int key;
+    cout << "Input the cypher key: ";
+    cin >> key;
+    bool positive = key > 0;
+    key = abs(key);
+    
+    string message;
+    cin.ignore();
+    getline(cin, message);
+    
+    int rows = ceil(validCharacters(message) / key);
+    char matrix[MAX_SIZE][MAX_SIZE];
+    
+    copyMessageToMatrix(message, matrix, key, rows, positive);
+    
+    for (int x = 0; x < rows; ++x)
     {
-        cout << "Currently inputting the elements of row " << x << ".\n";
-        for (int y = 0; y < size; ++y)
+        for (int y = 0; y < key; ++y)
         {
-            cin >> grid[x][y];
+            cout << matrix[x][y] << " ";
         }
+        cout << '\n';
     }
-    cout << "\n";
-
-    for (int depth = 0; depth < size / 2; ++depth)
-    {
-        for (int i = 0; i < 4; ++i)
-        {
-            for (int x = depth; x < size - depth - 1; ++x)
-            {
-                cout << grid[depth][x] << " ";
-            }
-            flipClockwise(grid, grid2, size);
-        }
-    }
-
+    
+    string encryptedMessage = encrypt(matrix, key, rows);
+    
+    cout << '\n' << encryptedMessage;
+    
     return 0;
 }
