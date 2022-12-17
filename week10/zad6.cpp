d1e68a4f579aaa405c8f148d2fecc9d4af7fcc53
IHaveNoIdeaWhatImDo-ing <84041839+IHaveNoIdeaWhatImDo-ing@users.noreply.github.com>
2022-12-17 04:51:36 -0800

    Create zad6.cpp

---
+++ week10/zad6.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+void replaceChar (char*& str, char& character, char& replacement)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == character)
+            str[i] = replacement;
+        
+        i++;
+    }
+}
+
+int main()
+{
+    char* str = new char[MAX_SIZE];
+    char character, replacement;
+    cout << "Enter the message: ";
+    cin >> str;
+    cout << "Enter the character to replace in the message: ";
+    cin >> character;
+    cout << "Enter a character to replace with in the message: ";
+    cin >> replacement;
+    
+    replaceChar(str, character, replacement);
+    
+    cout << '\n' << str;
+    
+    return 0;
+}
