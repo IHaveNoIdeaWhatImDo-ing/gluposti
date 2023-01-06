148993b80b8b8fcea465b579039ca25e5c247c66
IHaveNoIdeaWhatImDo-ing <84041839+IHaveNoIdeaWhatImDo-ing@users.noreply.github.com>
2023-01-06 08:44:44 +0200

    Create zad2.cpp

---
+++ week11/zad2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int fib (int n)
+{
+    if (n == 1)
+        return 0;
+    else if (n == 2)
+        return 1;
+    
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    cout << fib(14);
+
+    return 0;
+}
